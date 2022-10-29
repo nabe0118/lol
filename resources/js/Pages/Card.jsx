4496a891659bae68c18b59e6595a786e8ac3a945
渡邊直登 <watahen0118@gmail.com>
2022-10-30 03:34:28 +0900

    [add] cardコンポーネント

---
+++ resources/js/Pages/Card.jsx
@@ -0,0 +1,43 @@
+import { Link, Head } from '@inertiajs/inertia-react'
+import React from 'react'
+import LikeIcon from './LikeIcon'
+
+export default function Card() {
+  return (
+      <>
+          <div className="mt-2 flex">
+              <img
+                  className="mb-3 mr-2 h-24 w-24 rounded-full shadow-lg"
+                  src="/image/ChampSquares/AatroxSquare.png"
+                  alt="Bonnie image"
+              />
+              <div className="w-1/2 relative block rounded-lg border border-gray-200 bg-white p-6 shadow-md before:absolute before:top-1/4 before:-left-[15px] before:border-8 before:border-solid before:border-transparent before:border-r-gray-800 before:content-[''] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-200 dark:hover:bg-gray-700">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      タイトルが入ります
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                      紹介文が入ります
+                  </p>
+                  <LikeIcon></LikeIcon>
+              </div>
+          </div>
+          <div className="mt-2 flex">
+              <div className="mb-3 mr-2 h-24 w-24">
+              </div>
+              <div className="w-1/2 relative block rounded-lg border border-gray-200 bg-white p-6 shadow-md before:absolute before:top-1/4 before:-right-[15px] before:border-8 before:border-solid before:border-transparent before:border-l-gray-800 before:content-[''] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-200 dark:hover:bg-gray-700">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      タイトルが入ります
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                      紹介文が入ります
+                  </p>
+              </div>
+              <img
+                  className="mb-3 ml-2 h-24 w-24 rounded-full shadow-lg"
+                  src="/image/ChampSquares/AhriSquare.png"
+                  alt="Bonnie image"
+              />
+          </div>
+      </>
+  )
+}
